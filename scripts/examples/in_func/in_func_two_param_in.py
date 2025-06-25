f60b43555290e5a93dc607a40c52ccbe603a487d
K92hoang <kien.hoang@outlook.de>
2025-06-26 00:35:45 +0200

    Fixed function signature in in_func tests

--- scripts/examples/in_func/in_func_two_param_in.py
+++ scripts/examples/in_func/in_func_two_param_in.py
@@ -1,5 +1,5 @@
 """Tests behaviour of in-functions with two parameter as substring of one fixed string."""
-def in_func_two_param_in(param1, param2: str) -> bool:
+def in_func_two_param_in(param1: str, param2: str) -> bool:
     if param1 in "The fuzzingbook":
         return True
     if param2 in "The fuzzingbook":

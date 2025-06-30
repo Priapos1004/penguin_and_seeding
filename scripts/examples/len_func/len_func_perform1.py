717382c314115e3f92c8fda3ab262de160c0accf
K92hoang <kien.hoang@outlook.de>
2025-06-30 23:20:46 +0200

    Fixed len_func tests

--- scripts/examples/len_func/len_func_perform1.py
+++ scripts/examples/len_func/len_func_perform1.py
@@ -1,11 +1,9 @@
 """Tests behaviour of len-functions where the longer of two parameters has odd length and lengths are unequal."""
 def len_func_perform1(param1: str, param2: str) -> bool:
-    len1 = len(param1)
-    len2 = len(param2)
-    if len2 < len1:
-        if len1 % 2 == 1:
+    if len(param2) < len(param1):
+        if len(param1) % 2 == 1:
             return True
-    if len2 > len1:
-        if len2 % 2 == 1:
+    if len(param2) > len(param1):
+        if len(param2) % 2 == 1:
             return True
     return False
--- scripts/examples/len_func/len_func_perform2.py
+++ scripts/examples/len_func/len_func_perform2.py
@@ -1,8 +1,6 @@
 """Tests behaviour of len-functions with two parameters with a for loop."""
 def len_func_perform2(param1: str, param2: str) -> bool:
-    len1 = len(param1)
-    len2 = len(param2)  
-    for i in range(len1):        
-        if len2 - i > 6 and len1 > 6:
+    for i in range(len(param1)):        
+        if len(param2)  - i > 6 and len(param1) > 6:
             return True
     return False

800512c0787f6d954f800c7c3c4243ecab6ed057
K92hoang <kien.hoang@outlook.de>
2025-07-05 23:29:23 +0200

    Fixed Docstring

--- scripts/examples/str_with/endwith_2param.py
+++ scripts/examples/str_with/endwith_2param.py
@@ -1,4 +1,4 @@
-"""Tests behaviour when first string starts with 'temp_' or second ends with '.bak'."""
+"""Tests behaviour when first parameter ends with '.json' or second parameter ends with '.html'."""
 def endwith_2param(param1: str, param2: str) -> bool:
     if param1.endswith(".json"):
         return True

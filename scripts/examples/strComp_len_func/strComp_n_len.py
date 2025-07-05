07a4afe524bad041a2e2d917ca65d6d6aea214e5
K92hoang <kien.hoang@outlook.de>
2025-07-05 14:23:35 +0200

    Fixed dockstring and function name

--- scripts/examples/strComp_len_func/strComp_n_len.py
+++ scripts/examples/strComp_len_func/strComp_n_len.py
@@ -1,4 +1,4 @@
-"""Tests behaviour when a string matches one a specific value and its length does not exceed a given threshold."""
+"""Tests behaviour when a string matches one a specific value and its length does not exceed length of 30."""
 def strComp_n_len(param: str) -> bool:
     if (not param == "Software Engineering 1" and param == "Software Engineering 2") and len(param) < 30:
         return True

4e0b896a12ec4afd02393a5f4e23f7c408c173b1
Samuel Brinkmann <76175242+Priapos1004@users.noreply.github.com>
2025-07-05 16:45:27 +0200

    Changed Tests

--- scripts/examples/strComp_len_func/strComp_n_len.py
+++
@@ -1,5 +0,0 @@
-"""Tests behaviour when a string matches one a specific value and its length does not exceed length of 30."""
-def strComp_n_len(param: str) -> bool:
-    if (not param == "Software Engineering 1" and param == "Software Engineering 2") and len(param) < 30:
-        return True
-    return False
--- scripts/examples/strComp_len_func/strComp_n_len2.py
+++
@@ -1,5 +0,0 @@
-"""Tests behaviour of string matching with different length conditions on both inputs."""
-def strComp_n_len3(param1: str, param2: str) -> bool:
-    if (param1 == "Hello world!" and param2 == "Goodbye everyone.") and (len(param1) < len(param2)):
-        return True
-    return False

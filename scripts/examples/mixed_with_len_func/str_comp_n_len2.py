c29d06a78d64dab1b334b15383908c0fcafb6b78
K92hoang <kien.hoang@outlook.de>
2025-07-05 17:54:31 +0200

    Modified test

--- scripts/examples/mixed_with_len_func/str_comp_n_len2.py
+++ scripts/examples/mixed_with_len_func/str_comp_n_len2.py
@@ -1,5 +1,7 @@
-"""Tests behaviour based on multiple disjunctive conditions involving string equality and length checks."""
+"""Tests behaviour based on disjunctive and nested conditions involving string equality and length constraints."""
 def strComp_n_len4(param1: str, param2: str, param3: str) -> bool:    
-    if (param1 == "Prime number" or len(param2) == 13) or param3 != "Fibonacci" or len(param3) != 7:        
-        return True
+    if param1 == "Prime number" or len(param2) == 17:
+        if param3 == "Fibonacci" or len(param3) != 11:
+            if param2 == "Euler's number" or len(param1) > 23:                        
+                return True
     return False

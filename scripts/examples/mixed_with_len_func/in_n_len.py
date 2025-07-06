8a27fccb68a73e12ac177e6f5a03836758492c89
Samuel Brinkmann <76175242+Priapos1004@users.noreply.github.com>
2025-07-06 18:22:27 +0200

    Fixed naming error

--- scripts/examples/mixed_with_len_func/in_n_len.py
+++ scripts/examples/mixed_with_len_func/in_n_len.py
@@ -1,5 +1,5 @@
 """Tests behaviour when a substring matches a specific value and its length exceeds 10."""
-def strComp_n_len(param: str) -> bool:
+def in_n_len(param: str) -> bool:
     if (not param in "Software Engineering 1" and param in "Software Engineering 123") and len(param) > 10:  # noqa: E713
         return True
     return False
--- scripts/examples/mixed_with_len_func/str_comp_n_len.py
+++ scripts/examples/mixed_with_len_func/str_comp_n_len.py
@@ -1,7 +1,7 @@
 """Tests behaviour when the first parameter equals one of two values using or,
 has length modulo 3 equal to 2, and the second parameter is different from a forbidden value
 and has length greater than 8."""
-def strComp_n_len3(param1: str, param2: str) -> bool:    
+def str_comp_n_len(param1: str, param2: str) -> bool:    
     if ((param1 == "Software Engineering 1" or param1 == "Software Engineering 2")
         and len(param1) % 3 == 2) and (param2 != "forbidden" and len(param2) > 8):
         return True

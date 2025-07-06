253f8af2811a05bf8ed31960f450e9c6c72da144
K92hoang <kien.hoang@outlook.de>
2025-07-06 13:09:28 +0200

    Added a test with invalid options

--- scripts/examples/str_with/start_endwith_multi.py
+++ scripts/examples/str_with/start_endwith_multi.py
@@ -1,6 +1,6 @@
 """Tests behaviour with mixed start/end checks across two parameters using both or and nested if-statements."""
 def start_endwith_multi(param1: str, param2: str) -> bool:
-    if param1.startswith("wwww.") or param2.endswith("./login"):
+    if param1.startswith("www.") or param2.endswith("./login"):
         if param1.endswith(".com") and param2.startswith("https://"):
             return True
     return False

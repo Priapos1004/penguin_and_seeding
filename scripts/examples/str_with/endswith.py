27accdeb6cd5b761a4bf52ec3fd55b7516125378
Samuel Brinkmann <76175242+Priapos1004@users.noreply.github.com>
2025-07-15 16:18:32 +0200

    Added evaluation notebook
    - Added install requirements
    - Fixed naming of endswith basic test case

--- scripts/examples/str_with/endswith.py
+++ scripts/examples/str_with/endswith.py
@@ -1,5 +1,5 @@
 """Tests behaviour of in-functions with substring at the end."""
-def in_func_endswith(param: str) -> bool:
+def endswith(param: str) -> bool:
     if param.endswith("127.0.0.1"):
         return True
     return False
